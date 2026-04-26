c6bea4edcc4f6d41072800ab1309726c7e8eb3ce
FAR-ANALYST <analystfarouk@gmail.com>
2026-04-27 00:39:10 +0300

    Update app.py

--- app.py
+++ app.py
@@ -447,4 +447,4 @@ def err404(e):
 
 
 if __name__ == "__main__":
-    app.run(debug=True, host="0.0.0.0", port=int(os.environ.get("PORT", 5000)))
+    app.run(debug=false, host="0.0.0.0", port=int(os.environ.get("PORT", 5000)))
